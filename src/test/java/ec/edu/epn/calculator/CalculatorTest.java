1e40f9348f2d2ee69ebcdbcd83c44f4e801d3068
RicardoDaniel147 <ricardo.villarreal@epn.edu.ec>
2026-05-03 07:30:28 -0500

    Second COMMIT

--- src/test/java/ec/edu/epn/calculator/CalculatorTest.java
+++ src/test/java/ec/edu/epn/calculator/CalculatorTest.java
@@ -1,23 +1,65 @@
 package ec.edu.epn.calculator;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
 
+    /*
+     * Fases de una prueba unitaria:
+     * 1. Arrange
+     * 2. Act
+     * 3. Assert
+     */
+
     private Calculator calculator;
 
-    @Before
+    @BeforeEach
     public void setUp() {
+        // Arrange
         calculator = new Calculator();
     }
 
     @Test
+    @DisplayName("Test para la suma de dos números")
     public void testSum() {
-        assertEquals(4, calculator.sum(2, 2));
+        // Act + Assert
+        // assertEquals(4, calculator.sum(2, 2));
+        assertAll("Suma valores en varios casos",
+                () -> assertEquals(4, calculator.sum(2, 2)),
+                () -> assertEquals(5, calculator.sum(2, 3)),
+                () -> assertEquals(6, calculator.sum(3, 3)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 3",
+            "2, 3, 5",
+            "3, 4, 7"
+    })
+    public void testMultipleData(int a, int b, int expected) {
+        // arrange
+        calculator = new Calculator();
+        int result = calculator.sum(a, b);
+        // Assert
+        assertEquals(expected, result);
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3, 4, 5 })
+    public void testSumWithFixedValues(int value) {
+        // Arrange + Act
+        int result = calculator.sum(value, 0);
+        // Assert
+        assertTrue(value == result);
     }
 
     @Test
